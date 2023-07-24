c821ff8f3883677e2fb6e521a338de70a4580da7
765766546 <48751236+765766546@users.noreply.github.com>
2023-07-24 18:18:29 +0000

    Deploying to main from @ 765766546/qinglanyi-blog-v1@f0e16f195c1497c15497282e5d16c0d2e44ddda6 🚀

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "d515c7a40369d42c2079f9bc537e29d3"
+    "revision": "001588f998236d17c0754cfec16d43fc"
   },
   {
     "url": "assets/css/0.styles.827d369b.css",
@@ -54,8 +54,8 @@ self.__precacheManifest = [
     "revision": "5e4a2cfbc3aae83420146d71ee06ba17"
   },
   {
-    "url": "assets/js/1.3add3331.js",
-    "revision": "999cab65b14986cbdba029a38c5cacaf"
+    "url": "assets/js/1.a033d5bf.js",
+    "revision": "80e2986f15a0caa82869317e469c28b3"
   },
   {
     "url": "assets/js/10.0aa290f4.js",
@@ -94,20 +94,20 @@ self.__precacheManifest = [
     "revision": "a4ffb0cb0f3e31a0872d3fa24c0ed751"
   },
   {
-    "url": "assets/js/4.e43af5b5.js",
-    "revision": "ed530284163f0ec0aa932860fb7d9f30"
+    "url": "assets/js/4.078b31cf.js",
+    "revision": "f56de4ccad5d7923bcfbeb3042edaffc"
   },
   {
-    "url": "assets/js/5.978ff44c.js",
-    "revision": "6f91cbf05bc7c97c17029d499fc6936b"
+    "url": "assets/js/5.1faf4ee1.js",
+    "revision": "deac2383fcab3150b3c2783e071ed74f"
   },
   {
-    "url": "assets/js/6.42335c4e.js",
-    "revision": "5aaeebb1f161c2e10897785fff0f8ed8"
+    "url": "assets/js/6.691c7b93.js",
+    "revision": "afdccabc5b777038aa67402c06d30d47"
   },
   {
-    "url": "assets/js/7.b481c7c8.js",
-    "revision": "fd27c4add823abf2453fe59a4fccdb8d"
+    "url": "assets/js/7.f7bd8ed3.js",
+    "revision": "7a09e615517ee65bc3e0e7c32e23c136"
   },
   {
     "url": "assets/js/8.e2b10c8a.js",
@@ -118,8 +118,8 @@ self.__precacheManifest = [
     "revision": "dcec96f167eb5a1a8d5099b443bb2314"
   },
   {
-    "url": "assets/js/app.1fe73a8c.js",
-    "revision": "924393ee736bea5400af47b1fbf94dd9"
+    "url": "assets/js/app.f9159b3a.js",
+    "revision": "673e119dc3882c4b59342dbf13b41fc3"
   },
   {
     "url": "assets/js/vendors~flowchart.89c7dfd1.js",
@@ -127,59 +127,123 @@ self.__precacheManifest = [
   },
   {
     "url": "auto-deploy-blog-by-github-actions.html",
-    "revision": "9dd1c0914d400eed4bfb659f33206b59"
+    "revision": "bb5aaf3438f3e430c0c63e280867bf7f"
   },
   {
     "url": "categories/index.html",
-    "revision": "914fca566cfc2341e1af19080c2012f2"
+    "revision": "ba1d822dd4313733bd2f9cf7477a3f59"
   },
   {
     "url": "categories/工具/index.html",
-    "revision": "6464014b69c1442712c2c86efb998b05"
+    "revision": "7c152f2f902082272a9eaf395dda0b0d"
   },
   {
     "url": "categories/随笔/index.html",
-    "revision": "1a0652de4ff0cb64f2c5b5759e1394bf"
+    "revision": "0f31259b2e563b6815bd8a1104f1cdd0"
+  },
+  {
+    "url": "chunyou1.jpg",
+    "revision": "21d26b0ccab487f521bb7a949849d047"
+  },
+  {
+    "url": "chunyou2.jpg",
+    "revision": "acbe6d0668a6deb58f4e7cb6c932ed27"
+  },
+  {
+    "url": "chunyou3.jpg",
+    "revision": "38cd0a1f5b552a788a725cc6892ad484"
+  },
+  {
+    "url": "chunyou5.jpg",
+    "revision": "ece6f8e44870faaf0497e23743a390e6"
+  },
+  {
+    "url": "chunyou6.jpg",
+    "revision": "58b298edb34ecb6bb2b1e6c61f0fa7c3"
+  },
+  {
+    "url": "chunyou7.jpg",
+    "revision": "f3bef1abd2bd0bfed23296944886d058"
+  },
+  {
+    "url": "friend.jpg",
+    "revision": "95447d879af2d91219e97914ce6b358f"
   },
   {
     "url": "how-do-i-learn-python.html",
-    "revision": "5564ff4d2ac9285171fcd356e6b98767"
+    "revision": "336324759c9a3419f306da13f0e5a5c7"
   },
   {
     "url": "index.html",
-    "revision": "2f95cdb85e89265eba2e808dbddfefb7"
+    "revision": "0c9271961a80eb44dcc1140f3f42ffff"
+  },
+  {
+    "url": "lanlan-haitan.jpg",
+    "revision": "5d1de0bd5f7e1a59dbdced9353777f41"
+  },
+  {
+    "url": "lanlan-haitan.png",
+    "revision": "0a9c282df84db1e367d1365aacca73c7"
+  },
+  {
+    "url": "lanlan-haitan1.jpg",
+    "revision": "b221a9d8968e8e7f81e1e77f4bac3e15"
+  },
+  {
+    "url": "lanlan-haitan1(1).jpg",
+    "revision": "4d1337aa26a87975884453bc83da0fb0"
+  },
+  {
+    "url": "lianhuashan1.jpg",
+    "revision": "cb19a62d220c5bf931df1b50e0714f0d"
+  },
+  {
+    "url": "lianhuashan2.jpg",
+    "revision": "a0ce038b838a0a774d8f52c3845629ee"
+  },
+  {
+    "url": "lianhuashan3.jpg",
+    "revision": "520b74ba20fb524a827b90b20c8b6ce1"
+  },
+  {
+    "url": "lianhuashan4.jpg",
+    "revision": "5291e445fe7e3684fdb4f249cc3dabad"
+  },
+  {
+    "url": "logo.png",
+    "revision": "b138ea300294b50a9d87c41f31850a9b"
   },
   {
     "url": "tag/GitHub Actions/index.html",
-    "revision": "6abd618010aebba8d8ead0a3112f8454"
+    "revision": "78e6a20be7f2b98c7fd05ccaa7a38b08"
   },
   {
     "url": "tag/index.html",
-    "revision": "77bb79387d75b6038884a4d6256707c3"
+    "revision": "0f6a027674ed5a3198f82d2908422674"
   },
   {
     "url": "tag/Web Scraper/index.html",
-    "revision": "cec6e1219355957482b27f26a22d7af0"
+    "revision": "f19f81722986e1bbbcabbe16aaca9376"
   },
   {
     "url": "tag/博客/index.html",
-    "revision": "9002f0a242fba7ce69451bd04fa751b7"
+    "revision": "a43118f9308ac291d20f2172cc3ace20"
   },
   {
     "url": "tag/随笔/index.html",
-    "revision": "8107e0202120f801d43f6b2d1156644a"
+    "revision": "360feb4e7e37b9a28dfe97a34733a7c8"
   },
   {
     "url": "timeline/index.html",
-    "revision": "708cbd8bc5cc5761e25574ff170c2c48"
+    "revision": "6eedb1e1160a0013ecb0a804797c252e"
   },
   {
     "url": "vuepress-github-pages.html",
-    "revision": "c20faa8107c7cb439ad243db3d11f968"
+    "revision": "d79b82ed757c9315fd75c0fd90355d0a"
   },
   {
     "url": "web-scraper.html",
-    "revision": "7a2947691f1f7a9f3ef0ce59436f8519"
+    "revision": "eba29f825228b6c8cf76cb43e966393e"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
